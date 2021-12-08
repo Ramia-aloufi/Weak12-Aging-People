48202d21724377f7a660c37dd54b8eb4e1c95455
RAM111111 <r.r.99@windowslive.com>
2021-12-08 19:16:09 +0300

    Initial Commit

---
+++ Aging People/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aging People
+//
+//  Created by R on 04/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
